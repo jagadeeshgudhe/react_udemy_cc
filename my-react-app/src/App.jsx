0f48ba07788b102f9a3d461193cf047b3025ae29
Gudhe Jagadeesh <jagadeesh8808@gmail.com>
2024-12-19 00:19:15 +0530

    problem and solution

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+// Building & Using a Component
+// Your task is to create a new MainGoal component which outputs a paragraph of text that describes your main course goal (e.g., "My main goal: Learn React in-depth and from the ground up").
+// You'll have to create this new component from scratch and then use it inside the App component's JSX code.
+// (notice the "My main goal: Learn react from the ground up" text at the bottom)
+// Important: To ensure that the automatic checks correctly detect your solution, your component must be called MainGoal and it also must be exported. To do that, simply add the export keyword in front of your MainGoal component.
+// The MainGoal component also must contain the text "My main goal:" (followed by whatever your main course goal is).
 
+
+
+import React from 'react';
 
 export function MainGoal() {
   return <p>My main goal: Learn React in great detail.</p>;
 }
 
-// DO NOT EDIT THE APP COMPONENT NAME OR CONTENT
-// Except for outputting your custom component
+//existing code -|
 
 function App() {
   return (
